f8a1e0ec72f2725e37dbbb8c546559abfdae0c7d
m gema maulana <mgemamaulana04@gmail.com>
2023-07-22 14:24:58 +0700

    melakukan perubahan pada file html css dan js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const toggleTombol = document.getElementById('nav-togle');
 const navToggle = document.getElementById('nav');
+const main = document.getElementById('main');
 
 toggleTombol.addEventListener("click", function(){
     navToggle.classList.toggle("nav-togle");
+
+    navToggle.addEventListener("click", function(){
+        navToggle.classList.remove("nav-togle");
+    });
+
+    main.addEventListener("click", function(){
+        navToggle.classList.remove("nav-togle");
+    });
 })
