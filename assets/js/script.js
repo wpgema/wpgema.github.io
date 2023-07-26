dd8c8c945290f3b5d77189326c64fd88bb476009
m gema maulana <mgemamaulana04@gmail.com>
2023-07-26 14:04:18 +0700

    menambahkan fitur works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const toggleTombol = document.getElementById('nav-togle');
 const navToggle = document.getElementById('nav');
 const main = document.getElementById('main');
+const tagline = document.getElementById('tagline');
 
 toggleTombol.addEventListener("click", function(){
     navToggle.classList.toggle("nav-togle");
+    tagline.classList.toggle('margin-top')
 
     navToggle.addEventListener("click", function(){
         navToggle.classList.remove("nav-togle");
