29dec46c5f0029122ccc08846378abdabd0841ac
m gema maulana <mgemamaulana04@gmail.com>
2023-07-26 22:59:42 +0700

    memperbaiki file html css js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,15 @@ const tagline = document.getElementById('tagline');
 
 toggleTombol.addEventListener("click", function(){
     navToggle.classList.toggle("nav-togle");
-    tagline.classList.toggle('margin-top')
+    tagline.classList.toggle('margin-top');
 
     navToggle.addEventListener("click", function(){
         navToggle.classList.remove("nav-togle");
+        tagline.classList.remove('margin-top');
     });
 
     main.addEventListener("click", function(){
         navToggle.classList.remove("nav-togle");
+        tagline.classList.remove('margin-top');
     });
 })
