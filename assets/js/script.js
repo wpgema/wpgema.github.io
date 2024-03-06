3a77c02c155fad5428d09347023c364d160321a9
m gema maulana <mgemamaulana04@gmail.com>
2024-03-07 06:27:34 +0700

    melakukan migrasi pada website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,43 @@
-const toggleTombol = document.getElementById('nav-togle');
-const navToggle = document.getElementById('nav');
+const tombol = document.getElementById('tombol');
+const nav = document.getElementById('nav');
+const nav_links = document.querySelectorAll('.nav_link');
 const main = document.getElementById('main');
-const tagline = document.getElementById('tagline');
+const icons = document.querySelectorAll('i');
+const faqs = document.querySelectorAll('.faqs');
+const answer = document.querySelector('.answer')
 
-toggleTombol.addEventListener("click", function(){
-    navToggle.classList.toggle("nav-togle");
-    tagline.classList.toggle('margin-top');
+const arr = [nav, main];
 
-    navToggle.addEventListener("click", function(){
-        navToggle.classList.remove("nav-togle");
-        tagline.classList.remove('margin-top');
-    });
+nav_links.forEach(n_l => {
+    n_l.addEventListener('click', function(){
 
-    main.addEventListener("click", function(){
-        navToggle.classList.remove("nav-togle");
-        tagline.classList.remove('margin-top');
-    });
+        document.querySelector('.nav_active')?.classList.remove('nav_active');
+        n_l.classList.add('nav_active')
+
+    })
+
+})
+
+tombol.addEventListener('click', function(){
+    nav.classList.toggle('nav-toggle');
+
+    setTimeout(function(){
+        icons[0].classList.toggle('bi-list')
+        icons[0].classList.toggle('bi-x')
+    }, 500)
+
+});
+
+for(let i = 0; i < arr.length; i++){
+    arr[i].addEventListener('click', function(){
+        nav.classList.remove('nav-toggle');
+        icons[0].classList.remove('bi-x')
+        icons[0].classList.add('bi-list')
+    })    
+}
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', function(){
+        faq.classList.toggle('active')
+    })
 })
